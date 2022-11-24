6359b5fa19c28d3bc58bf89724904d7a8f33d78c
maulana <maulaadjis52@gmail.com>
2022-11-24 13:15:56 +0700

    first commit

---
+++ on-going-anime.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Head -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Web | Maulana adji sentosa</title>
+    <link rel="stylesheet" href="bootstraps/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="bootstraps/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="fontawesome/css/all.min.css" type="text/css">
+</head>
+<!-- End Head -->
+
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">SekaiSubs</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end text-bg-primary" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">SekaiSubs</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="on-going-anime.php">Anime On-going</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="batch-anime.php">Anime Batch</a>
+                        </li>
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" href="index.php">Sosial Media</a>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Cara Download</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Pencarian..." aria-label="Search">
+                        <button class="btn btn-primary" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- End Navbar -->
+
+    <div class="card-body container bg-dark text-white">
+        <!-- <section id="1"> -->
+        <div id="pertama" class="container d-flex align-items-center bg-secondary my-3 p-2">
+            <div class="fw-bold">
+                Daftar On-Going Anime
+            </div>
+            <div class="ms-auto bg-info">
+                <a class="btn text-white">Anime On-Going</a>
+            </div>
+        </div>
+
+        <div class="container text-center mb-2">
+            <div class="row mb-3 pb-2">
+                <a href="#" class="col-md-6 my-2">
+                    <img src="gambar/1.jpg" class="img-fluid" alt="...">
+                </a>
+                <a href="#" class="col-md-6 my-2">
+                    <img src="gambar/1.jpg" class="img-fluid" alt="...">
+                </a>
+                <a href="#" class="col-md-6 my-2">
+                    <img src="gambar/1.jpg" class="img-fluid" alt="...">
+                </a>
+                <a href="#" class="col-md-6 my-2">
+                    <img src="gambar/1.jpg" class="img-fluid" alt="...">
+                </a>
+                <a href="#" class="col-md-6 my-2">
+                    <img src="gambar/1.jpg" class="img-fluid" alt="...">
+                </a>
+                <a href="#" class="col-md-6 my-2">
+                    <img src="gambar/1.jpg" class="img-fluid" alt="...">
+                </a>
+                <a href="#" class="col-md-6 my-2">
+                    <img src="gambar/1.jpg" class="img-fluid" alt="...">
+                </a>
+                <a href="#" class="col-md-6 my-2">
+                    <img src="gambar/1.jpg" class="img-fluid" alt="...">
+                </a>
+                <a href="#" class="col-md-6 my-2">
+                    <img src="gambar/1.jpg" class="img-fluid" alt="...">
+                </a>
+                <a href="#" class="col-md-6 my-2">
+                    <img src="gambar/1.jpg" class="img-fluid" alt="...">
+                </a>
+            </div>
+        </div>
+        <!-- </section> -->
+    </div>
+
+    <footer>
+        <div>
+            <small>All right reserved © Copyright 2022 - TPQ</small>
+        </div>
+    </footer>
+
+</body>
